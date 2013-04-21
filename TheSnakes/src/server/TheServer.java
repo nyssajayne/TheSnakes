a0fc26a9c2c60eeb2ea2bf9f2386edd8a412c82f
nyssajayne <nyssajayne@gmail.com>
2013-04-21 16:48:40 +1000

    this might just need to be ignored, setting up Eclipse to run with GitHub.

---
+++ TheSnakes/src/server/TheServer.java
@@ -0,0 +1,43 @@
+package server;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+import labFour.LoanCalculations;
+import labFour.TheServer;
+
+public class TheServer {
+	
+	private DataInputStream input;
+	private DataOutputStream output;
+	
+	public TheServer()
+	{
+		try
+		{
+			ServerSocket serverSocket = new ServerSocket(1985);
+			System.out.println("New Server at " + new Date());
+			
+			//This would accept just one snake
+			Socket socket = serverSocket.accept();
+			
+			input = new DataInputStream(socket.getInputStream());
+			output = new DataOutputStream(socket.getOutputStream());
+			
+		}
+		catch (IOException e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		new TheServer();
+	}
+
+}
