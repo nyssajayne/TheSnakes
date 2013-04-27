224c53fdbf9fa60f6201f87262f45d5cc935febf
Rob <s3238801@student.rmit.edu.au>
2013-04-27 20:09:55 +1000

    adding the interface

---
+++ TheSnakes/src/server/model/SnakeInterface.java
@@ -0,0 +1,11 @@
+package server.model;
+
+public interface SnakeInterface {
+	public static int MOVE_NONE = 0;
+	public static int MOVE_LEFT = 1;
+	public static int MOVE_RIGHT = 2;
+	public static int MOVE_FASTER = 3;
+	public static int MOVE_SLOWER = 4;
+	public static int MOVE_EXIT = 5;
+	public static int NO_PLAYER = -1;
+}
